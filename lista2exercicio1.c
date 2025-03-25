c4c484606523e6a7a17a05d1f32dbe2adf2bc832
MarcDevGuy127 <marceloh.github@gmail.com>
2025-03-24 22:09:01 -0300

    Resolução #1 lista2exercicio1

---
+++ lista2exercicio1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+int main()
+{
+    char palavra[20];
+    printf("Digite uma palavra: ");
+    scanf("%19s", palavra); // Remove o operador & e limita para evitar overflow
+
+    for (int i = 0; i < strlen(palavra); i++)
+    {
+        printf("%c", toupper(palavra[i])); // Usa %c para caracteres individuais
+    }
+    printf("\n");
+    for (int i = 0; i < strlen(palavra); i++)
+    {
+        printf("%c", tolower(palavra[i])); // Usa %c para caracteres individuais
+    }
+    printf("\nSTRUCPR: %s\n", strupr(palavra));
+
+    return 0;
+}
