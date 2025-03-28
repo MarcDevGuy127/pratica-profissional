565b4a8dbc633cad2ed7a57ec4e154ad098b5573
MarcDevGuy127 <marceloh.github@gmail.com>
2025-03-28 11:12:24 -0300

    Resolução #1 lista2exercicio3.c

---
+++ lista2exercicio3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char substring[10], palavra[20];
+
+    printf("Digite uma palavra: ");
+    scanf("%19s", &palavra);
+
+    printf("Digite uma parte da palavra: ");
+    scanf("%9s", &substring);
+    
+    printf("Palavra: %s \n", palavra);
+    printf("Substring: %s \n", substring);
+
+    int tamanhoPalavra = strlen(palavra);
+    int tamanhoSub = strlen(substring);
+
+    for (int i = 0; i <= tamanhoPalavra - tamanhoSub; i++) {
+        if (strncmp(&palavra[i], substring, tamanhoSub) == 0) {
+            printf("Substring encontrada na posição: %d\n", i);
+        }
+    }
+    return 0;
+}
