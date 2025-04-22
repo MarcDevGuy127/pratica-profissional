7da35a29ce74ba9746defeca62292009dff030cc
MarcDevGuy127 <marceloh.github@gmail.com>
2025-04-22 19:45:20 -0300

    Conservando bugs de lista2exercicio8

--- jogo_da_forca/lista2exercicio8.c
+++ jogo_da_forca/lista2exercicio8.c
@@ -3,112 +3,58 @@
 #include <ctype.h>
 
 // Função para imprimir o desenho da forca com base no número de erros
-void imprimirDesenhoForca(int erros)
-{
+void imprimirDesenhoForca(int erros) {
     const char *forca[] = {
-        "  _______     \n"
-        " |/      |    \n"
-        " |            \n"
-        " |            \n"
-        " |            \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |            \n"
-        " |            \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |       |    \n"
-        " |            \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |      \\|    \n"
-        " |            \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |      \\|/   \n"
-        " |            \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |      \\|/   \n"
-        " |       |    \n"
-        " |            \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |      \\|/   \n"
-        " |       |    \n"
-        " |      /     \n"
-        "_|___         \n",
-
-        "  _______     \n"
-        " |/      |    \n"
-        " |      (_)   \n"
-        " |      \\|/   \n"
-        " |       |    \n"
-        " |      / \\   \n"
-        "_|___         \n"};
+        "  _______     \n |/      |    \n |            \n |            \n |            \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |            \n |            \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |       |    \n |            \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |      \\|    \n |            \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |      \\|/   \n |            \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |      \\|/   \n |       |    \n |            \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |      \\|/   \n |       |    \n |      /     \n_|___         \n",
+        "  _______     \n |/      |    \n |      (_)   \n |      \\|/   \n |       |    \n |      / \\   \n_|___         \n"
+    };
 
     printf("%s\n", forca[erros]);
 }
 
 // Função para imprimir a tela do jogo
-void imprimirTelaJogo(int erros, const char *palavraParcial, const char *letrasTentativas)
-{
+void imprimirTelaJogo(int erros, const char *palavraParcial, const char *letrasTentativas) {
     printf("========== JOGO DA FORCA ==========\n\n");
-
     imprimirDesenhoForca(erros);
-
     printf("Palavra: %s\n", palavraParcial);
     printf("Letras tentadas: %s\n", letrasTentativas);
     printf("Erros: %d de 7\n", erros);
     printf("===================================\n\n");
 }
 
-// Função que conta as letras faltando
-int contarLetrasFaltando(const char *palavra, const char *letrasAdivinhadas)
-{
-    int letrasFaltando = 0;
-
-    // Percorre a palavra secreta
-    for (int i = 0; i < strlen(palavra); i++) {
-        if (palavra[i] != ' ' && !strchr(letrasAdivinhadas, tolower(palavra[i]))) {
-            letrasFaltando++;
+// Função para verificar se letra já foi tentada
+int letraJaTentada(char letra, const char *letrasTentativas) {
+    for (int i = 0; i < strlen(letrasTentativas); i++) {
+        if (tolower(letrasTentativas[i]) == tolower(letra)) {
+            return 1;
         }
     }
-
-    return letrasFaltando;
+    return 0;
 }
 
 // Função para processar a tentativa de uma letra
-void tentativasLetras(char *palavra, char letra, int *erros, char *letrasTentativas, int *letrasDescobertas)
-{
+int tentativasLetras(char *palavra, char letra, int *erros, char *letrasTentativas) {
     int letraEncontrada = 0;
+
+    if (letraJaTentada(letra, letrasTentativas)) {
+        printf("Você já tentou essa letra. Tente outra!\n");
+        return 0;
+    }
+
+    // Adiciona a letra na lista de tentativas
+    int tam = strlen(letrasTentativas);
+    letrasTentativas[tam] = toupper(letra);
+    letrasTentativas[tam + 1] = '\0';
+
     for (int i = 0; i < strlen(palavra); i++) {
-        if (tolower(letra) == tolower(palavra[i])) {
+        if (toupper(letra) == toupper(palavra[i])) {
             letraEncontrada = 1;
-            (*letrasDescobertas)++;
         }
     }
 
@@ -116,41 +62,40 @@ void tentativasLetras(char *palavra, char letra, int *erros, char *letrasTentati
         (*erros)++;
     }
 
-    // Adiciona a letra na lista de tentativas
-    int tam = strlen(letrasTentativas);
-    letrasTentativas[tam] = letra;
-    letrasTentativas[tam + 1] = '\0';
+    return letraEncontrada;
 }
 
-int main()
-{
+int main() {
     int erros = 0;
-    int letrasDescobertas = 0;  // Contador para letras descobertas
     char palavra[] = "PATO";
     char letra;
-    char letrasTentativas[30] = "";  // Lista de letras tentadas
-    char palavraParcial[] = "_ _ _ _";  // Palavra parcial para exibir
+    char letrasTentativas[30] = "";
+    char palavraParcial[2 * sizeof(palavra)];  // espaço com underline + espaços
+    int letrasDescobertas = 0;
+
+    // Inicializa a palavra parcial com underlines
+    for (int i = 0, j = 0; i < strlen(palavra); i++, j += 2) {
+        palavraParcial[j] = '_';
+        palavraParcial[j + 1] = ' ';
+    }
+    palavraParcial[2 * strlen(palavra)] = '\0';
 
     while (erros < 7 && letrasDescobertas < strlen(palavra)) {
-        // Imprime a tela do jogo
         imprimirTelaJogo(erros, palavraParcial, letrasTentativas);
 
-        // Lê uma nova tentativa
         printf("Digite uma letra: ");
         scanf(" %c", &letra);
 
-        // Processa a tentativa de letra
-        tentativasLetras(palavra, letra, &erros, letrasTentativas, &letrasDescobertas);
+        int encontrou = tentativasLetras(palavra, letra, &erros, letrasTentativas);
 
-        // Atualiza a palavra parcial
-        for (int i = 0; i < strlen(palavra); i++) {
-            if (strchr(letrasTentativas, tolower(palavra[i]))) {
-                palavraParcial[i * 2] = palavra[i];  // Atualiza a letra
+        if (encontrou) {
+            for (int i = 0; i < strlen(palavra); i++) {
+                if (toupper(letra) == toupper(palavra[i])) {
+                    palavraParcial[i * 2] = palavra[i];
+                    letrasDescobertas++;
+                }
             }
         }
-
-        // Exibe o progresso
-        printf("\nPalavra atual: %s\n", palavraParcial);
     }
 
     if (erros == 7) {
