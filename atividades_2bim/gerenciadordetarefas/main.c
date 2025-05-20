f729af47939e5b1dc56fafb128c66b710a240595
MarcDevGuy127 <marceloh.github@gmail.com>
2025-05-20 20:18:19 -0300

    Separando printf's

--- atividades_2bim/gerenciadordetarefas/main.c
+++ atividades_2bim/gerenciadordetarefas/main.c
@@ -8,6 +8,8 @@
   #define CLEAR_CMD "clear" // Comando para limpar tela em Unix-like
 #endif
 
+#define MAX_CHAR 120 //Limitando numero de caracteres
+
 // Função que chama o sistema para limpar a tela
 void clearScreen() {
     system(CLEAR_CMD);
@@ -29,18 +31,39 @@ void funcaoImprimirTela() {
 int main()
 {
     int opcao;
+    char texto[MAX_CHAR];
     funcaoImprimirTela();
-    scanf("%d", &opcao);
+    scanf("%d", &opcao); //<-- adicionar testes de entrada
 
-    clearScreen();
+    clearScreen(); //limpat tela
 
     switch (opcao) {
         case 1:
-            printf("A");
+            printf("Escreva a tarefa que voce deseja adicionar:\n");
+            scanf("%c", &texto);
+            printf("A tarefa '%c' foi adicionada!", &texto);
             getchar();
             break;
         case 2:
-            printf("BB");
+            printf("Listando tarefas disponiveis:\n");
+            getchar();
+            break;
+        case 3:
+            printf("Digite a tarefa que deseja consultar:\n");
+            getchar();
+            break;
+        case 4:
+            printf("Ordenando as tarefas disponiveis:\n");
+            getchar();
+            break;
+        case 5:
+            printf("Exibindo tarefas disponiveis:\n");
+            //funcao() <--local para reutilizar funcao para exibir tarefas registradas.
+            printf("Selecione o numero de uma tarefa para realizar edicao:\n");
+            scanf("%d", &opcao);
+            printf("A seguinte tarefa foi selecionada para edicao:"); //ver se da para adicionar uma condicional igual o popup Voce tem certeza? S ou N
+
+            //printf(scanf("%c", &texto)); //deixar o texto placeholder da tarefa escolhida para editar na digitaçao
             getchar();
             break;
         default:
